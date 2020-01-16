f3f25651b10b7f7ab35fc9706a1ba95fef01d892
neerajp99 <neerajp1999@gmail.com>
2020-01-16 08:02:36 +0530

    Add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, {Component} from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import socketIOClient from "socket.io-client";
+import Landing from "./components/Landing"
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Route />
-      </Router>
-    </div>
-  );
+class App extends Component {
+
+
+    state = {
+      response: false,
+      endpoint: "http://127.0.0.1:5009"
+    };
+
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("FromAPIs", data => this.setState({ response: data }));
+  }
+  render() {
+    const { response } = this.state;
+    return (
+          <div className="app">
+            <Router>
+              <Route exact path = "/" render ={() => <Landing />} />
+            </Router>
+
+
+          </div>
+    )
+  }
 }
-
 export default App;
