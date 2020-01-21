3b19de5efd81d97a21a6c32503fa4749e9ef69f1
neerajp99 <neerajp1999@gmail.com>
2020-01-21 11:34:13 +0530

    Establish database connection

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const http = require("http");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// Bring in Users route
+const users = require("./routes/api/users");
+
+// bring in database connection keys
+const keys = require("./config/keys");
 
 // Initialise express app
 const app = require("express")();
@@ -8,31 +15,58 @@ const app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-// Initialise Port number
-const port = process.env.PORT || 5009;
+// Add body-parser middleware to get body details
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
 
+// Home route
 app.get("/", (req, res) => {
   res.json("jajja");
 });
 
 // New connection
 io.on("connection", socket => {
-  console.log("New client connected", socket.id), setInterval(
-    () => getApiAndEmit(socket),
-    100
-  );
+  console.log("New client connected", socket.id),
+    setInterval(() => getApiAndEmit(socket), 100);
   socket.on("disconnect", () => console.log("Client disconnected"));
 });
 
 // make the getApiAndEmit method
-const getApiAndEmit = (socket) => {
-  try{
-    let x = Math.random()
-    socket.emit("FromAPI", socket.id+x)
-  } catch (error){
-    console.log(error)
+const getApiAndEmit = socket => {
+  try {
+    let x = Math.random();
+    socket.emit("FromAPI", socket.id + x);
+  } catch (error) {
+    console.log(error);
   }
-}
+};
+
+// Establish a database connection
+const db = keys.mongoURI;
+mongoose
+  .connect(
+    db,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(res => {
+    console.log("Database connected successfully!");
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+// Call routes
+app.use("/api/users", users);
+
+// Initialise Port number
+const port = process.env.PORT || 5009;
 
 // Listen to the app
 server.listen(port, () => {
