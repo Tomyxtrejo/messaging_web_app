727efa50b967ea655a8c19e92ae007210a9163e9
neerajp99 <neerajp1999@gmail.com>
2020-01-21 17:55:45 +0530

    Add store

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,11 @@ import socketIOClient from "socket.io-client";
 import Landing from "./components/Landing";
 import Register from "./components/authentication/Register";
 import Login from "./components/authentication/Login";
+import store from "./store";
+import { Provider } from "react-redux";
+import setAuthToken from "./utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+import { setCurrentUser, logoutUser } from "./actions/authAction";
 
 class App extends Component {
   state = {
@@ -13,19 +18,41 @@ class App extends Component {
   };
 
   componentDidMount() {
-    const { endpoint } = this.state;
-    const socket = socketIOClient(endpoint);
-    socket.on("FromAPIs", data => this.setState({ response: data }));
+    // const { endpoint } = this.state;
+    // const socket = socketIOClient(endpoint);
+    // socket.on("FromAPIs", data => this.setState({ response: data }));
+
+    // Check if the user is already present in localstorage
+    // if yes, then authenticate the user
+    if (localStorage.authToken) {
+      // Set auth token header auth
+      setAuthToken(localStorage.authToken);
+      //Decode the tokens ang get user infe ormation
+      const decodedToken = jwt_decode(localStorage.authToken);
+      // Set user and isAuthenticated
+      store.dispatch(setCurrentUser(decodedToken));
+      //Check for expired tokens
+      const currentTime = Date.now() / 1000;
+      if (decodedToken.exp < currentTime) {
+        // log out user
+        store.dispatch(logoutUser());
+        // Redirect to login
+        window.alert("Session expired, please login again.");
+        window.location.href = "/login";
+      }
+    }
   }
   render() {
     const { response } = this.state;
     return (
       <div className="app">
-        <Router>
-          <Route exact path="/" render={() => <Landing />} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path = "/login" component = {Login} />
-        </Router>
+        <Provider store={store}>
+          <Router>
+            <Route exact path="/" render={() => <Landing />} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Router>
+        </Provider>
       </div>
     );
   }
