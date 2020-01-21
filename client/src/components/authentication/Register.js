9d3e6aba1be323d4e2ff1c057449f60cbcb6d421
neerajp99 <neerajp1999@gmail.com>
2020-01-21 17:57:29 +0530

    Add method to register

--- client/src/components/authentication/Register.js
+++ client/src/components/authentication/Register.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import InputField from "../commons/InputField";
 
-import google from "../../utils/img/google.png"
+import google from "../../utils/img/google.png";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { registerUser } from "../../actions/authAction";
+import PropTypes from "prop-types";
 
 class Register extends Component {
   // State goes here
@@ -18,6 +22,32 @@ class Register extends Component {
       [event.target.name]: event.target.value
     });
   };
+
+  // Trigger this function when user submits the form
+  onSubmit = event => {
+    event.preventDefault();
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password
+    };
+    this.props.registerUser(newUser, this.props.history);
+  };
+  // Lifecycle to method to check if the user is already authenticated
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/");
+    }
+  }
+
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/");
+    }
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
   render() {
     return (
       <div className="register">
@@ -56,11 +86,14 @@ class Register extends Component {
                 onChange={this.onChange}
                 label="Confirm Password"
               />
-              <button className="register_details_submit_button btn btn-primary btn-lg">
+              <button
+                className="register_details_submit_button btn btn-primary btn-lg"
+                onClick={this.onSubmit}
+              >
                 Register
               </button>
               <button className="register_details_google2 register_details_google  btn btn-primary btn-lg">
-              <img src={google}  className="register_details_google_icon"/>
+                <img src={google} className="register_details_google_icon" />
                 LOGIN WITH GOOGLE
               </button>
             </div>
@@ -72,4 +105,17 @@ class Register extends Component {
   }
 }
 
-export default Register;
+// Add prop types
+Register.propTypes = {
+  auth: PropTypes.object.isRequired,
+  registerUser: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { registerUser }
+)(withRouter(Register));
