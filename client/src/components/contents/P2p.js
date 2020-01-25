25bd1209c0dd53992a8ea486b4e720800d038012
neerajp99 <neerajp1999@gmail.com>
2020-01-25 21:47:16 +0530

    Add options and privacy collapsable accordions

--- client/src/components/contents/P2p.js
+++ client/src/components/contents/P2p.js
@@ -11,49 +11,157 @@ class P2p extends Component {
             src={three}
             alt="Profile Image"
           />
-          <div class="card-body profile_card_body">
+          <div className="card-body profile_card_body">
             <h5 className="card-title">Katherine Harders</h5>
             <p className="card-text">@katy_hardy</p>
           </div>
         </div>
         {""}
-        <input type="checkbox" name="toggle" id="options_toggle" />
-        <label for="options_toggle" id="privacy_toggle_label"/>
+        {/* Accordion starts here */}
+        <div className="container">
+          <div className="accordion-option">
+            <a
+              href="javascript:void(0)"
+              className="toggle-accordion active"
+              accordion-id="#accordion"
+            />
+          </div>
+          <div className="clearfix" />
+          <div
+            className="panel-group"
+            id="accordion"
+            role="tablist"
+            aria-multiselectable="true"
+          >
+            <div className="panel panel-default p2p_profile_details_accordion">
+              <div
+                className="panel-heading"
+                role="tab"
+                id="headingOne"
+                role="button"
+                data-toggle="collapse"
+                data-parent="#accordion"
+                href="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                <h4 className="panel-title p2p_profile_details_accordion_title">
+                  <a
+                    role="button"
+                    data-toggle="collapse"
+                    data-parent="#accordion"
+                    href="#collapseOne"
+                    aria-expanded="true"
+                    aria-controls="collapseOne"
+                  >
+                    OPTIONS
+                  </a>
+                </h4>
+              </div>
+              <div
+                id="collapseOne"
+                className="panel-collapse collapse in"
+                role="tabpanel"
+                aria-labelledby="headingOne"
+              >
+                <div className="panel-body">
+                  <div className="p2p-panel-body-list">
+                    <div className="p2p-panel-body-list-item">
+                      <div className="p2p-panel-body-list-item-details">
+                        Edit Nickname
+                      </div>
+                      <div className="p2p-panel-body-list-item-icon">
+                        <i className="fas fas fa-edit panel-edit-icon" />
+                      </div>
+                    </div>
 
-        <div className="options_toggle_div">
-          <i className="fa fa-chevron-down down-chevron-2" />
+                    <div className="p2p-panel-body-list-item">
+                      <div className="p2p-panel-body-list-item-details">
+                        Change Theme
+                      </div>
+                      <div className="p2p-panel-body-list-item-icon">
+                        <i className="fas fa-tint panel-tint-icon" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="panel panel-default p2p_profile_details_accordion">
+              <div
+                className="panel-heading"
+                role="tab"
+                id="headingTwo"
+                data-toggle="collapse"
+                data-parent="#accordion"
+                href="#collapseTwo"
+                aria-expanded="true"
+                aria-controls="collapseTwo"
+              >
+                <h4 className="panel-title p2p_profile_details_accordion_title">
+                  <a
+                    className="collapsed"
+                    role="button"
+                    data-toggle="collapse"
+                    data-parent="#accordion"
+                    href="#collapseTwo"
+                    aria-expanded="true"
+                    aria-controls="collapseTwo"
+                  >
+                    PRIVACY AND SUPPORT
+                  </a>
+                </h4>
+              </div>
+              <div
+                id="collapseTwo"
+                className="panel-collapse collapse in"
+                role="tabpanel"
+                aria-labelledby="headingTwo"
+              >
+                <div className="panel-body">
+                  <div className="p2p-panel-body-list">
+                    <div className="p2p-panel-body-list-item">
+                      <div className="p2p-panel-body-list-item-details">
+                        Notifications
+                      </div>
+                      <div className="p2p-panel-body-list-item-icon">
+                        <i className="fas fa-bell panel-bell-icon" />
+                      </div>
+                    </div>
+
+                    <div className="p2p-panel-body-list-item">
+                      <div className="p2p-panel-body-list-item-details">
+                        Ignore Messages
+                      </div>
+                      <div className="p2p-panel-body-list-item-icon">
+                        <i className="fas fa-cut panel-reject-icon" />
+                      </div>
+                    </div>
+
+                    <div className="p2p-panel-body-list-item">
+                      <div className="p2p-panel-body-list-item-details">
+                        Block User
+                      </div>
+                      <div className="p2p-panel-body-list-item-icon">
+                        <i className="fas fa-ban panel-block-icon" />
+                      </div>
+                    </div>
+
+                    <div className="p2p-panel-body-list-item">
+                      <div className="p2p-panel-body-list-item-details">
+                        Something went wrong?
+                      </div>
+                      <div className="p2p-panel-body-list-item-icon">
+                        <i className="fas fa-exclamation-triangle panel-warning-icon" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="p2p_options" id = "p2p_options">
-          <div className="p2p_options_content">
-            <div className="p2p_options_content_friends_icon">
-              <i className="fa fa-user-circle p2p_options_content_friends_icon_i" />
-            </div>
-            <div className="p2p_options_content_friends_since">
-              Friends since August 2018.
-            </div>
-          </div>
-
-          <div className="p2p_options_content">
-            <div className="p2p_options_content_friends_icon">
-              <i className="fa fa-paper-plane p2p_options_content_friends_icon_i" />
-            </div>
-            <div className="p2p_options_content_friends_since">
-              katy_hardy@gmail.com
-            </div>
-          </div>
-
-          <div className="p2p_options_content">
-            <div className="p2p_options_content_friends_icon">
-              <i className="fa fa-phone p2p_options_content_friends_icon_i" />
-            </div>
-            <div className="p2p_options_content_friends_since">
-              +91-84557-36445
-            </div>
-          </div>
-        </div>
-        {""}
-        <button className="lala">alalala</button>
-
+        {/* Accordion ends here */}
       </div>
     );
   }
