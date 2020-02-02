10ce289f993cb313970f7397c5e25760ce6e9141
neerajp99 <neerajp1999@gmail.com>
2020-02-02 21:46:07 +0530

    Update user model

--- models/Users.js
+++ models/Users.js
@@ -17,39 +17,39 @@ const UserSchema = new Schema({
   createdDate: {
     type: Date,
     default: Date.now()
-  }
-  // friends: [
-  //   {
-  //     name: {
-  //       type: String,
-  //       required: true
-  //     },
-  //     email: {
-  //       type: String,
-  //       required: true
-  //     },
-  //     unique_id: {
-  //       type: String,
-  //       required: true
-  //     }
-  //   }
-  // ],
-  // friendRequest: [
-  //   {
-  //     name: {
-  //       type: String,
-  //       required: true
-  //     },
-  //     email: {
-  //       type: String,
-  //       required: true
-  //     },
-  //     unique_id: {
-  //       type: String,
-  //       required: true
-  //     }
-  //   }
-  // ]
+  },
+  friends: [
+    {
+      name: {
+        type: String,
+        required: true
+      },
+      email: {
+        type: String,
+        required: true
+      },
+      unique_id: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  friendRequest: [
+    {
+      name: {
+        type: String,
+        required: true
+      },
+      email: {
+        type: String,
+        required: true
+      },
+      unique_id: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
 
 module.exports = User = mongoose.model("users", UserSchema);
